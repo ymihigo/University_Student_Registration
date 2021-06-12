ab8cf806bfbfbaae3b46ea87052855bae0e17bb5
Captain <Captain@DESKTOP-9CC1HJA>
2021-06-12 15:34:24 +0200

    V1

--- University_Intake/src/main/java/com/Mihigo/university_intake/view/ViewControll.java
+++ University_Intake/src/main/java/com/Mihigo/university_intake/view/ViewControll.java
@@ -2,12 +2,27 @@ package com.Mihigo.university_intake.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ViewControll {
 	
+	
+	@RequestMapping("/")
 	public String root(Model model) {
 		return "index";
+	}
+	
+	@RequestMapping("/newStudent")
+	public String newStudent(Model model) {
+		return "NewStudent";
+	}
+	
+	
+	@RequestMapping("/newFaculty")
+	public String newFaculty(Model model) {
+		return "FacultyForm";
 	}
 	
 }
